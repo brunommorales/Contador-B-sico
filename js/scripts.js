19bfb17b782838bae5e1049752ff09e4a6c12a28
Bruno <bruno,morales99@icloud.com>
2022-06-29 19:26:13 -0300

    contador-basico

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+var currentNumberWrapper = document.getElementById("currentNumber");
+var currentNumber = 1;
+
+function increment() {
+    currentNumber = currentNumber + 1;
+    currentNumberWrapper.innerHTML = currentNumber;
+}
+
+function decrement() {
+    currentNumber = currentNumber - 1;
+    currentNumberWrapper.innerHTML = currentNumber;
+}
